d222fd616c59e012b2d57a5e979e943c0e138f44
Jung Hyun Nam <rkttu@rkttu.com>
2021-02-19 15:32:39 +0900

    Add AudioCrossFadeFilter and FadeCurves

---
+++ src/EasyFFmpegFilters/AudioCrossFadeFilter.cs
@@ -0,0 +1,37 @@
+﻿namespace EasyFFmpeg
+{
+    public class AudioCrossFadeFilter : AudioFilter
+    {
+        public override string Name => "acrossfade";
+
+        /// <summary>
+        /// Specify the number of samples for which the cross fade effect has to last. At the end of the cross fade effect the first input audio will be completely silent. Default is 44100.
+        /// </summary>
+        [FilterOptionName("ns", 44100)]
+        public int? NumberOfSamples { get; set; } = default;
+
+        /// <summary>
+        /// Specify the duration of the cross fade effect. See (ffmpeg-utils)the Time duration section in the ffmpeg-utils(1) manual for the accepted syntax. By default the duration is determined by nb_samples. If set this option is used instead of nb_samples.
+        /// </summary>
+        [FilterOptionName("d", null)]
+        public float? Duration { get; set; } = default;
+
+        /// <summary>
+        /// Should first stream end overlap with second stream start. Default is enabled.
+        /// </summary>
+        [FilterOptionName("o", 1, 0, 1)]
+        public int? Overlap { get; set; } = default;
+
+        /// <summary>
+        /// Set curve for cross fade transition for first stream.
+        /// </summary>
+        [FilterOptionName("curve1", AudioFadeCurve.Tri)]
+        public AudioFadeCurve? Curve1 { get; set; } = default;
+
+        /// <summary>
+        /// Set curve for cross fade transition for second stream.
+        /// </summary>
+        [FilterOptionName("curve1", AudioFadeCurve.Tri)]
+        public AudioFadeCurve? Curve2 { get; set; } = default;
+    }
+}
---
+++ src/EasyFFmpegFilters/AudioFadeCurve.cs
@@ -0,0 +1,125 @@
+﻿namespace EasyFFmpeg
+{
+    public enum AudioFadeCurve
+    {
+        /// <summary>
+        /// select triangular, linear slope (default)
+        /// </summary>
+        [EnumStringValue("tri")]
+        Tri,
+
+        /// <summary>
+        /// select quarter of sine wave
+        /// </summary>
+        [EnumStringValue("qsin")]
+        Qsin,
+
+        /// <summary>
+        /// select half of sine wave
+        /// </summary>
+        [EnumStringValue("hsin")]
+        Hsin,
+
+        /// <summary>
+        /// select exponential sine wave
+        /// </summary>
+        [EnumStringValue("esin")]
+        Esin,
+
+        /// <summary>
+        /// select logarithmic
+        /// </summary>
+        [EnumStringValue("log")]
+        Log,
+
+        /// <summary>
+        /// select inverted parabola
+        /// </summary>
+        [EnumStringValue("ipar")]
+        Ipar,
+
+        /// <summary>
+        /// select quadratic
+        /// </summary>
+        [EnumStringValue("qua")]
+        Qua,
+
+        /// <summary>
+        /// select cubic
+        /// </summary>
+        [EnumStringValue("cub")]
+        Cub,
+
+        /// <summary>
+        /// select square root
+        /// </summary>
+        [EnumStringValue("squ")]
+        Squ,
+
+        /// <summary>
+        /// select cubic root
+        /// </summary>
+        [EnumStringValue("cbr")]
+        Cbr,
+
+        /// <summary>
+        /// select parabola
+        /// </summary>
+        [EnumStringValue("par")]
+        Par,
+
+        /// <summary>
+        /// select exponential
+        /// </summary>
+        [EnumStringValue("exp")]
+        Exp,
+
+        /// <summary>
+        /// select inverted quarter of sine wave
+        /// </summary>
+        [EnumStringValue("iqsin")]
+        Iqsin,
+
+        /// <summary>
+        /// select inverted half of sine wave
+        /// </summary>
+        [EnumStringValue("ihsin")]
+        Ihsin,
+
+        /// <summary>
+        /// select double-exponential seat
+        /// </summary>
+        [EnumStringValue("dese")]
+        Dese,
+
+        /// <summary>
+        /// select double-exponential sigmoid
+        /// </summary>
+        [EnumStringValue("desi")]
+        Desi,
+
+        /// <summary>
+        /// select logistic sigmoid
+        /// </summary>
+        [EnumStringValue("losi")]
+        Losi,
+
+        /// <summary>
+        /// select sine cardinal function
+        /// </summary>
+        [EnumStringValue("sinc")]
+        Sinc,
+
+        /// <summary>
+        /// select inverted sine cardinal function
+        /// </summary>
+        [EnumStringValue("isinc")]
+        Isinc,
+
+        /// <summary>
+        /// no fade applied
+        /// </summary>
+        [EnumStringValue("nofade")]
+        NoFade,
+    }
+}
