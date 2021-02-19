d4134a95e7200c27413b61b6038448998bd08a9e
Jung Hyun Nam <rkttu@rkttu.com>
2021-02-19 15:32:07 +0900

    Add AudioCrossOver Filter

---
+++ src/EasyFFmpegFilters/AudioCrossoverFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyFFmpeg
+{
+    public class AudioCrossoverFilter : AudioFilter
+    {
+        public override string Name => "acrossover";
+
+        /// <summary>
+        /// Set split frequencies. Those must be positive and increasing.
+        /// </summary>
+        [FilterOptionName("split", null)]
+        public int[] Split { get; set; } = default;
+
+        /// <summary>
+        /// Set filter order for each band split. This controls filter roll-off or steepness of filter transfer function.
+        /// </summary>
+        [FilterOptionName("order", AudioCrossoverOrder._4th)]
+        public AudioCrossoverOrder? Order { get; set; } = default;
+
+        /// <summary>
+        /// Set input gain level. Allowed range is from 0 to 1. Default value is 1.
+        /// </summary>
+        [FilterOptionName("level", 1f, 0f, 1f)]
+        public float? Level { get; set; } = default;
+
+        /// <summary>
+        /// Set output gain for each band. Default value is 1 for all bands.
+        /// </summary>
+        [FilterOptionName("gains", 1)]
+        public int? Gains { get; set; } = default;
+    }
+}
---
+++ src/EasyFFmpegFilters/AudioCrossoverOrder.cs
@@ -0,0 +1,65 @@
+﻿namespace EasyFFmpeg
+{
+    public enum AudioCrossoverOrder
+    {
+        /// <summary>
+        /// Default value. 24 dB per octave.
+        /// </summary>
+        [EnumStringValue("4th")]
+        _4th,
+
+        /// <summary>
+        /// 12 dB per octave.
+        /// </summary>
+        [EnumStringValue("2nd")]
+        _2nd,
+
+        /// <summary>
+        /// 36 dB per octave.
+        /// </summary>
+        [EnumStringValue("6th")]
+        _6th,
+
+        /// <summary>
+        /// 48 dB per octave.
+        /// </summary>
+        [EnumStringValue("8th")]
+        _8th,
+
+        /// <summary>
+        /// 60 dB per octave.
+        /// </summary>
+        [EnumStringValue("10th")]
+        _10th,
+
+        /// <summary>
+        /// 72 dB per octave.
+        /// </summary>
+        [EnumStringValue("12th")]
+        _12th,
+
+        /// <summary>
+        /// 84 dB per octave.
+        /// </summary>
+        [EnumStringValue("14th")]
+        _14th,
+
+        /// <summary>
+        /// 96 dB per octave.
+        /// </summary>
+        [EnumStringValue("16th")]
+        _16th,
+
+        /// <summary>
+        /// 108 dB per octave.
+        /// </summary>
+        [EnumStringValue("18th")]
+        _18th,
+
+        /// <summary>
+        /// 120 dB per octave.
+        /// </summary>
+        [EnumStringValue("20th")]
+        _20th,
+    }
+}
