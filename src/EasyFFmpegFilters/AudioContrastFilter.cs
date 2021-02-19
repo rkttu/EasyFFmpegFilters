a08f8374b15bac931d272c4d7a5bc92af0e925dd
Jung Hyun Nam <rkttu@rkttu.com>
2021-02-19 15:32:22 +0900

    Add AudioContrastFilter

---
+++ src/EasyFFmpegFilters/AudioContrastFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyFFmpeg
+{
+    /// <summary>
+    /// Simple audio dynamic range compression/expansion filter.
+    /// </summary>
+    public class AudioContrastFilter : AudioFilter
+    {
+        public override string Name => "aconstrast";
+
+        [FilterOptionName("contrast", 33, 0, 100)]
+        public int? Contrast { get; set; } = default;
+    }
+}
