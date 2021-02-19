c309f1e81ae3691fd609748d85fc573c8f51652e
Jung Hyun Nam <rkttu@rkttu.com>
2021-02-19 15:32:15 +0900

    Add AudioCopyFilter

---
+++ src/EasyFFmpegFilters/AudioCopyFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyFFmpeg
+{
+    /// <summary>
+    /// Copy the input audio source unchanged to the output. This is mainly useful for testing purposes.
+    /// </summary>
+    public class AudioCopyFilter : AudioFilter
+    {
+        public override string Name => "acopy";
+    }
+}
